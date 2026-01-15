4e296971d1440ab4cca6835c950b90598d3496a0
marcello <marcellorusso.x@gmai.com>
2026-01-15 11:22:41 +0100

    Add experiment runner for training orchestration

---
+++ src/kladml/training/__init__.py
@@ -0,0 +1,11 @@
+"""
+KladML Training Module
+
+Training execution components with dependency injection.
+"""
+
+from kladml.training.runner import ExperimentRunner
+
+__all__ = [
+    "ExperimentRunner",
+]
